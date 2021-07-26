857a842e40273aae52f919317b58edd8264005bb
Roisin <roisinmaguire@ma359.home>
2021-07-26 17:15:43 +0100

    Chapter 8 - add in_journey component

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,2 +1,29 @@
 class Oystercard
-end 
+
+  attr_reader :balance, :LIMIT, :in_journey
+  LIMIT = 90
+
+  def initialize
+    @balance = 0
+    @in_journey = false
+
+  end
+
+  def top_up(money)
+    fail "Top up would exceed limit of #{LIMIT}." if (@balance + money > 90)
+    @balance += money
+  end
+
+  def deduct(fare)
+    @balance -= fare
+  end
+
+  def journey?
+    @in_journey
+  end
+
+  def touch_in
+    @in_journey = true
+  end
+
+end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,3 +1,34 @@
+require 'oystercard.rb'
+
 describe Oystercard do
+
+  it { is_expected.to respond_to(:top_up).with(1).argument }
+
+  it 'adds money to the card balance' do
+    expect(subject.top_up(10)).to eq (10)
+  end
+
+  it 'can top up the balance' do
+    expect{ subject.top_up 1 }.to change{ subject.balance }.by 1
+  end
+
+  it 'prevents topping up balance if it would go over the limit' do
+    limit = Oystercard::LIMIT
+    subject.top_up(limit)
+    expect { subject.top_up 1 }.to raise_error "Top up would exceed limit of #{limit}."
+  end
+
+  it 'can deduct the balance' do
+    expect{ subject.deduct 1 }.to change{ subject.balance }.by -1
+  end
+
+  it 'starts not in journey' do
+    expect(subject.journey?).to eq false
+  end
+
+  it 'starts journey when user touches in' do
+    subject.touch_in
+    expect(subject.journey?).to eq true
+  end
 
 end
