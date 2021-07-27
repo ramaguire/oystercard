6adfa35fcc6e34ef8f69eb7f05420c34e7c6b63a
Roisin <roisinmaguire@ma359.home>
2021-07-27 15:48:55 +0100

    saving the entry station

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,11 +1,13 @@
 class Oystercard
 
-  attr_reader :balance, :LIMIT, :in_journey
+  attr_reader :balance, :LIMIT, :in_journey, :MINIMUM_FARE, :entry_station
   LIMIT = 90
+  MINIMUM_FARE = 1
 
   def initialize
     @balance = 0
     @in_journey = false
+    @entry_station
 
   end
 
@@ -14,16 +16,28 @@ class Oystercard
     @balance += money
   end
 
-  def deduct(fare)
-    @balance -= fare
-  end
-
   def journey?
-    @in_journey
+    if @entry_station
+      true
+    else
+      false
+    end
   end
 
-  def touch_in
-    @in_journey = true
+  def touch_in(station)
+    fail 'Not enough funds to travel' unless balance >= MINIMUM_FARE
+    @entry_station = (station)
   end
+
+  def touch_out
+    deduct(MINIMUM_FARE)
+    @entry_station = nil
+  end
+
+  private
+
+    def deduct(fare)
+      @balance -= fare
+    end
 
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,34 +1,71 @@
 require 'oystercard.rb'
 
 describe Oystercard do
+  subject(:card) { described_class.new }
+  let(:limit) { Oystercard::LIMIT }
+  let(:station) { double :station }
 
   it { is_expected.to respond_to(:top_up).with(1).argument }
 
-  it 'adds money to the card balance' do
-    expect(subject.top_up(10)).to eq (10)
+  describe '#top_up' do
+    it 'adds money to the card balance' do
+      expect(card.top_up(10)).to eq (10)
+    end
+
+    it 'can top up the balance' do
+      expect{ card.top_up 1 }.to change{ card.balance }.by 1
+    end
+
+    it 'prevents topping up balance if it would go over the limit' do
+      card.top_up(limit)
+      expect { card.top_up 1 }.to raise_error "Top up would exceed limit of #{limit}."
+    end
   end
 
-  it 'can top up the balance' do
-    expect{ subject.top_up 1 }.to change{ subject.balance }.by 1
-  end
-
-  it 'prevents topping up balance if it would go over the limit' do
-    limit = Oystercard::LIMIT
-    subject.top_up(limit)
-    expect { subject.top_up 1 }.to raise_error "Top up would exceed limit of #{limit}."
-  end
-
-  it 'can deduct the balance' do
-    expect{ subject.deduct 1 }.to change{ subject.balance }.by -1
-  end
+  # it 'can deduct the balance' do
+  #   expect{ card.deduct 1 }.to change{ card.balance }.by -1
+  # end
 
   it 'starts not in journey' do
-    expect(subject.journey?).to eq false
+    expect(card.journey?).to eq false
   end
 
-  it 'starts journey when user touches in' do
-    subject.touch_in
-    expect(subject.journey?).to eq true
+  describe '#touch_in' do
+
+    it 'starts journey when user touches in' do
+      card.top_up(limit)
+      card.touch_in(station)
+      expect(card.journey?).to eq true
+    end
+
+    it 'prevents user touching in if balance is less than £1' do
+      expect{ card.touch_in(station) }.to raise_error 'Not enough funds to travel'
+    end
+
+    it 'remembers the entry station after touch in' do
+      card.top_up(limit)
+      expect { card.touch_in(station) }.to change {card.entry_station}.to station
+    end
+  end
+
+  describe '#touch_out' do
+    it 'ends journey when user touches out' do
+      card.top_up(limit)
+      card.touch_in(station)
+      card.touch_out
+      expect(card.journey?).to eq false
+    end
+
+    it 'deduces minimum fare when user touches out' do
+      minimum_fare = Oystercard::MINIMUM_FARE
+      expect { card.touch_out }.to change {card.balance}.by -minimum_fare
+    end
+
+    it 'forgets the entry station after touch out' do
+      card.top_up(limit)
+      card.touch_in(station)
+      expect { card.touch_out }.to change {card.entry_station}.to nil
+    end
   end
 
 end
